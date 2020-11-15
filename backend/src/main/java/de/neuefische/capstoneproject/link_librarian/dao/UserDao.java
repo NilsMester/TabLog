e4e4c8d241678dd99eaeed8b2c7639ebba260b88
Nils Mester <66268838+NilsMester@users.noreply.github.com>
2020-11-15 11:55:39 +0100

    Revert "B - add UserLogin with Spring-Security"

--- backend/src/main/java/de/neuefische/capstoneproject/link_librarian/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package de.neuefische.capstoneproject.link_librarian.dao;
-
-
-import de.neuefische.capstoneproject.link_librarian.model.LinkLibrarianUser;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserDao extends PagingAndSortingRepository<LinkLibrarianUser, String> {
-}
